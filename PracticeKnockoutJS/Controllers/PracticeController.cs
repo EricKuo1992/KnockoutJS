8eb3f029cc109a07e2732dd0b644cbe4dadaf1b2
EricKuo <breaking3310@gmail.com>
2021-06-30 00:10:14 +0800

    基礎環境設置

---
+++ PracticeKnockoutJS/Controllers/PracticeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeKnockoutJS.Controllers
+{
+    public class PracticeController : Controller
+    {
+        // GET: Practice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PracticeKnockoutJS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticeKnockoutJS.MvcApplication" Language="C#" %>
